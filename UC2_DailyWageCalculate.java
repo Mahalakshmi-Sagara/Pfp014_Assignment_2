bd083f2207e2d5de28aee0b0d0a4fee78d17a8ae
Maha <maharangnath17@gmail.com>
2023-12-19 13:17:22 +0530

    DailyWageCalculate Added

---
+++ UC2_DailyWageCalculate.java
@@ -0,0 +1,13 @@
+package Pfp014_Assignment_2;
+
+public class UC2_DailyWageCalculate {
+
+	public static void main(String[] args) {
+		int WagePerHour = 20;
+		int FullDayHour = 8;
+		int DailyWage = WagePerHour*FullDayHour;
+		System.out.println("Daily employee wage: " + DailyWage);
+
+	}
+
+}
